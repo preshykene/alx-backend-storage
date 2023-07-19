eddca973bcd57e193ae8dc8a4f956ccb8f752fa8
preshykene <preciousekene51@gmail.com>
2023-07-19 18:02:39 +0000

    Done with this

---
+++ 0x02-redis_basic/web.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""Implementing an expiring web cache and tracker"""
+import redis
+import requests
+from functools import wraps
+from typing import Callable
+
+
+redis_store = redis.Redis()
+"""redis storage
+"""
+
+
+def data_cacher(method: Callable) -> Callable:
+    """wrapper function
+    """
+    @wraps(method)
+    def invoker(url) -> str:
+        """ wrapper function
+        """
+        redis_store.incr(f'count:{url}')
+        result = redis_store.get(f'result:{url}')
+        if result:
+            return result.decode('utf-8')
+        result = method(url)
+        redis_store.set(f'count:{url}', 0)
+        redis_store.setex(f'result:{url}', 10, result)
+        return result
+    return invoker
+
+
+@data_cacher
+def get_page(url: str) -> str:
+    """return URL
+    """
+    return requests.get(url).text
